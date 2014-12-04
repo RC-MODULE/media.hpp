a0b7fe7f1eb46d6ca8f42d7e4eb3ed2fe2e6a2c3
Kirill Mikhailov <kirill.mikhailov@gmail.com>
2014-12-04 12:11:59 +0300

    h264: added msvd wrapper for h264 slice decoder
    Signed-off-by: Kirill Mikhailov <kirill.mikhailov@gmail.com>

--- msvd.hpp
+++ msvd.hpp
@@ -2,6 +2,8 @@
 #define __MSVD_HPP_5cab0e5f_3058_498e_857a_a1e6bfea1e98__
 
 #include "mpeg.hpp"
+#include "h264-syntax.hpp"
+#include "h264-slice.hpp"
 #include "bitstream.hpp"
 #include <asio.hpp>
 #include <asio/system_timer.hpp>
@@ -191,7 +193,199 @@ auto async_decode_picture(
     callback);
 }
 
+
+namespace detail {
+
+msvd_picture_type to_msvd(h264::picture_type p) {
+  switch(p) {
+  case h264::picture_type::frame: return msvd_picture_type_frame;
+  case h264::picture_type::top: return msvd_picture_type_top;
+  case h264::picture_type::bot: return msvd_picture_type_bot;
+  }
+}
+
+msvd_coding_type to_msvd(h264::coding_type p) {
+  switch(p) {
+  case h264::coding_type::P: return msvd_coding_type_P;
+  case h264::coding_type::B: return msvd_coding_type_B;
+  case h264::coding_type::I: return msvd_coding_type_I;
+  default: throw std::runtime_error("unsupported coding_type"); 
+  }
+}
+
+template<typename Sequence> 
+struct h264_context : msvd_h264_decode_params {
+  template<typename I, typename Frame>
+  h264_context(
+    h264::seq_parameter_set const& sps,
+    h264::pic_parameter_set const& pps,
+    h264::slice_header const& slice,
+    I frames_begin, I frames_end,
+    Frame const& curr_pic,
+    Sequence const& slice_data,
+    std::size_t slice_data_offset)
+  {
+    using namespace h264;
+
+    memset(this, 0, sizeof(*this));
+
+    using frame_buffer_type = typename std::decay<decltype(frame_buffer(curr_pic))>::type;
+
+    geometry = {
+      buffer_traits<frame_buffer_type>::width,
+      buffer_traits<frame_buffer_type>::height,
+      buffer_traits<frame_buffer_type>::luma_offset,
+      buffer_traits<frame_buffer_type>::chroma_offset
+    };
+
+    decoded_picture_buffer_size = std::transform(frames_begin, frames_end, dpb_data, 
+      [&](decltype(*frames_begin)& f) { 
+        if(is_reference(top(f)) || is_reference(bot(f)))
+          return msvd_h264_frame{phys_addr(frame_buffer(f)), (std::int16_t)TopFieldOrderCnt(f), (std::int16_t)BotFieldOrderCnt(f)};
+        else
+          return msvd_h264_frame{phys_addr(frame_buffer(curr_pic)), (std::int16_t)TopFieldOrderCnt(curr_pic), (std::int16_t)BotFieldOrderCnt(curr_pic)}; 
+      }) - dpb_data;
+    decoded_picture_buffer = dpb_data;
+
+    std::array<picture_reference<I>, 32> rl[2];
+    picture_reference<I>* rl_end[2] = {rl[0].begin(), rl[1].begin()};
+
+    if(slice.slice_type == coding_type::P) {
+      rl_end[0] = generate_reflist_for_p_slice(MaxFrameNum(sps), curr_pic, utils::make_range(frames_begin, frames_end),
+        utils::make_range(slice.ref_pic_list_modification[0]),
+        rl[0].begin(), slice.num_ref_idx_l0_active_minus1 + 1);
+    }
+    else if(slice.slice_type == coding_type::B) {
+      std::tie(rl_end[0], rl_end[1]) = 
+        generate_reflists_for_b_slice(MaxFrameNum(sps), curr_pic, utils::make_range(frames_begin, frames_end),
+          utils::make_range(slice.ref_pic_list_modification[0]), 
+          utils::make_range(slice.ref_pic_list_modification[1]),
+          rl[0].begin(), slice.num_ref_idx_l0_active_minus1 + 1,
+          rl[1].begin(), slice.num_ref_idx_l1_active_minus1 + 1);
+    }
+
+    for(auto i = 0; i != 2; ++i) {
+      reflist[i].size = rl_end[i] - rl[i].begin();
+      reflist[i].data = reflist_data[i];
+
+      auto to_msvd_reference = [&](picture_reference<I> const& r, slice_header::weight_pred_table_element const& w) {
+        return msvd_h264_picture_reference{
+          to_msvd(pic_type(r)), std::size_t(r.frame - frames_begin), is_long_term_reference(r),
+          {w.luma.weight, w.luma.offset},
+          {w.cb.weight, w.cb.offset},
+          {w.cr.weight, w.cr.offset}
+        }; 
+      }; 
+
+      if(!slice.weight_pred_table[i].empty())
+        std::transform(rl[i].begin(), rl_end[i],  slice.weight_pred_table[i].begin(), reflist_data[i], to_msvd_reference);
+      else
+        std::transform(rl[i].begin(), rl_end[i], reflist_data[i], [&](picture_reference<I> const& r) {
+          return to_msvd_reference(r, {
+            {std::int8_t(1 << slice.luma_log2_weight_denom), 0},
+            {std::int8_t(1 << slice.chroma_log2_weight_denom), 0},
+            {std::int8_t(1 << slice.chroma_log2_weight_denom), 0} 
+          }); 
+        });
+    } 
+  
+    col_pic_type = msvd_picture_type_frame; 
+    if(rl_end[1] != rl[1].begin()) {
+      if(field_flag(*rl[1][0].frame))
+        col_pic_type = /*(rl[1][0].pt == h264::picture_type::frame) ? msvd_picture_type_top :*/ to_msvd(rl[1][0].pt);
+      
+      col_abs_diff_poc_flag = abs(TopFieldOrderCnt(*rl[1][0].frame) - PicOrderCnt(curr_pic)) >= abs(BotFieldOrderCnt(*rl[1][0].frame) - PicOrderCnt(curr_pic));
+    }
+  
+    hor_pic_size_in_mbs            = sps.pic_width_in_mbs_minus1 + 1;
+    vert_pic_size_in_mbs           = (sps.pic_height_in_map_units_minus1 + 1) * (2 - sps.frame_mbs_only_flag);
+    mb_mode                        = sps.chroma_format_idc == 1 ? 0 : 1;
+    frame_mbs_only_flag            = sps.frame_mbs_only_flag;
+    mbaff_frame_flag               = sps.mb_adaptive_frame_field_flag;
+    direct_8x8_inference_flag      = sps.direct_8x8_inference_flag;
+    max_num_ref_frames             = sps.max_num_ref_frames;
+
+    if(pps.scaling_matrix) 
+      scaling_list                 = reinterpret_cast<msvd_h264_scaling_lists const*>(&*pps.scaling_matrix);
+    else if(sps.scaling_matrix)
+      scaling_list                 = reinterpret_cast<msvd_h264_scaling_lists const*>(&*sps.scaling_matrix);
+    else
+      scaling_list = 0;
+  
+    constr_intra_pred_flag         = pps.constrained_intra_pred_flag;    
+    transform_8x8_mode_flag        = pps.transform_8x8_mode_flag;
+    entropy_coding_mode_flag       = pps.entropy_coding_mode_flag;
+    weight_mode                    = slice.slice_type == coding_type::B ? pps.weighted_bipred_idc : pps.weighted_pred_flag;
+    chroma_qp_index_offset         = pps.chroma_qp_index_offset;
+    second_chroma_qp_index_offset  = pps.second_chroma_qp_index_offset;
+
+    picture_type                   = to_msvd(slice.pic_type);
+    slice_type                     = to_msvd(slice.slice_type);
+
+    first_mb_in_slice              = slice.first_mb_in_slice;
+    cabac_init_idc                 = slice.cabac_init_idc;
+    disable_deblocking_filter_idc  = slice.disable_deblocking_filter_idc;
+    slice_qpy                      = 26 + pps.pic_init_qp_minus26 + slice.slice_qp_delta;
+    direct_spatial_mv_pred_flag    = slice.direct_spatial_mv_pred_flag;
+    luma_log2_weight_denom         = slice.luma_log2_weight_denom;
+    chroma_log2_weight_denom       = slice.chroma_log2_weight_denom;
+
+    this->curr_pic                 = {phys_addr(frame_buffer(curr_pic)), (std::int16_t)TopFieldOrderCnt(curr_pic), (std::int16_t)BotFieldOrderCnt(curr_pic)}; 
+    
+    buffers = bitstream::adapt_sequence(slice_data); 
+    this->slice_data = &*buffers.begin();
+    slice_data_n = buffers.size();
+    this->slice_data_offset = slice_data_offset;
+  }
+
+  msvd_h264_frame dpb_data[17];
+  msvd_h264_picture_reference reflist_data[2][32];
+  decltype(bitstream::adapt_sequence(std::declval<Sequence>())) buffers;
+
+  msvd_decode_result result;
+};
+
+template<typename S, typename F>
+auto async_decode_slice(decoder& d, std::unique_ptr<h264_context<S>> cx, F callback) ->
+  typename std::enable_if<utils::is_callable<F(std::error_code, msvd::decode_result)>::value>::type
+{
+  auto ctx = cx.get();
+  auto mc = utils::move_on_copy(std::move(cx));
+
+  utils::async_write_some(d.fd, utils::make_ioctl_write_buffer<MSVD_DECODE_H264_SLICE>(std::ref(*ctx)))
+  >> [ctx,&d](std::size_t) {
+    return utils::async_read_some(d.fd, asio::mutable_buffers_1(&ctx->result, sizeof(ctx->result)));
+  }
+  >> [mc](std::size_t bytes) {
+    return decode_result{unwrap(mc)->result.num_of_decoded_mbs};
+  }
+  += callback;
+} 
+
+} // namespace detail
+
+template<typename C, typename I, typename Sequence, typename F>
+auto async_decode_slice(
+  decoder& d,
+  h264::seq_parameter_set const& sps,
+  h264::pic_parameter_set const& pps,
+  h264::slice_header const& header,
+  C const& curr_pic,
+  I dpb_begin, I dpb_end,
+  Sequence const& slice_data,
+  std::size_t slice_data_offset,
+  F callback) -> typename std::enable_if<utils::is_callable<F(std::error_code, msvd::decode_result)>::value>::type
+{
+  detail::async_decode_slice(
+    d, 
+    std::unique_ptr<detail::h264_context<Sequence>>(
+      new detail::h264_context<Sequence>(sps, pps, header, dpb_begin, dpb_end, curr_pic, slice_data, slice_data_offset)
+    ),
+    callback);
+}
+
 } // namespace msvd
+
 
 #endif
 
