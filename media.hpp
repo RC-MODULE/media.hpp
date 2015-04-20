7f18fa9d2711ddc141d297e881368db16eec0290
Kirill Mikhailov <kmikhailov@module.ru>
2015-04-20 12:43:47 +0300

    added media.hpp header
    Signed-off-by: Kirill Mikhailov <kmikhailov@module.ru>

---
+++ media.hpp
@@ -0,0 +1,14 @@
+#ifndef __media_hpp__010b332b_1059_46e7_9d2a_72eddc758b38__
+#define __media_hpp__010b332b_1059_46e7_9d2a_72eddc758b38__
+
+#include "utils.hpp"
+#include "mpeg.hpp"
+#include "ts.hpp"
+#include "clock.hpp"
+#include "video.hpp"
+#include "h264-syntax.hpp"
+#include "h264-slice.hpp"
+#include "h264-context.hpp"
+#include "mpeg-decoder.hpp"
+
+#endif
